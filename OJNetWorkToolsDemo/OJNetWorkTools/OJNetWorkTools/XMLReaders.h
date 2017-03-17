729c331eb0b45ccc7d332126b930a8bbd01acb1b
One'J <OneJ@OneJ.local>
2017-03-17 23:50:48 +0800

    Release 1.0.0.

---
+++ OJNetWorkToolsDemo/OJNetWorkTools/OJNetWorkTools/XMLReaders.h
@@ -0,0 +1,26 @@
+//
+//  OJNetWorkTools.h
+//  OJNetWorkTools
+//
+//  Created by OneJ on 2017/3/17.
+//  Copyright © 2017年 Onej. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef NS_OPTIONS(NSUInteger, XMLReaderOptions){
+    XMLReaderOptionsProcessNamespaces           = 1 << 0, // Specifies whether the receiver reports the namespace and the qualified name of an element.
+    XMLReaderOptionsReportNamespacePrefixes     = 1 << 1, // Specifies whether the receiver reports the scope of namespace declarations.
+    XMLReaderOptionsResolveExternalEntities     = 1 << 2, // Specifies whether the receiver reports declarations of external entities.
+};
+
+
+@interface XMLReaders : NSObject <NSXMLParserDelegate>
+
++ (NSDictionary *)dictionaryForXMLData:(NSData *)data error:(NSError **)errorPointer;
++ (NSDictionary *)dictionaryForXMLString:(NSString *)string error:(NSError **)errorPointer;
++ (NSDictionary *)dictionaryForXMLData:(NSData *)data options:(XMLReaderOptions)options error:(NSError **)errorPointer;
++ (NSDictionary *)dictionaryForXMLString:(NSString *)string options:(XMLReaderOptions)options error:(NSError **)errorPointer;
+
+
+@end
